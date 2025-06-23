9e1cf1731def1606963debbc1e5fec815b41de62
sumit504 <112240812+sumit504@users.noreply.github.com>
2025-06-23 14:25:27 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,14 @@
+// import React from 'react'; ← REMOVE THIS
+
+import CollageCreator from './components/CollageCreator';
+
+function App() {
+  return (
+    <div>
+      <h1>Farcaster Collage Mini App</h1>
+      <CollageCreator />
+    </div>
+  );
+}
+
+export default App;
